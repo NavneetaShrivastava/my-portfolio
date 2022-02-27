17ea54319552d2d1b375d661128c5cecc12dd3a7
shubhanshudhagat <shrivastavanavneeta@gmail.com>
2022-02-27 22:33:54 +0800

    Updates

---
+++ precache-manifest.31bfa88488b32ca72e7b60bca2a3c795.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19dc38957207e6691e806b378eb62748",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "4e22a8a86ee8a61052da",
+    "url": "/my-portfolio/static/css/2.6280b9c9.chunk.css"
+  },
+  {
+    "revision": "e98ed622604f367356ee",
+    "url": "/my-portfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "4e22a8a86ee8a61052da",
+    "url": "/my-portfolio/static/js/2.227c004e.chunk.js"
+  },
+  {
+    "revision": "2d3407f67e1ce6854fbe05878e8577b3",
+    "url": "/my-portfolio/static/js/2.227c004e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e98ed622604f367356ee",
+    "url": "/my-portfolio/static/js/main.44938e06.chunk.js"
+  },
+  {
+    "revision": "28d97e6c4183a7c87d51",
+    "url": "/my-portfolio/static/js/runtime-main.9994cf37.js"
+  }
+]);
