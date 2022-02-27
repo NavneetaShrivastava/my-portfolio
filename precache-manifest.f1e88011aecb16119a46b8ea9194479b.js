1ba18b4974947c95c3088f649db452956cc8851c
shubhanshudhagat <shrivastavanavneeta@gmail.com>
2022-02-27 22:38:48 +0800

    Updates

--- precache-manifest.f1e88011aecb16119a46b8ea9194479b.js
+++ precache-manifest.f1e88011aecb16119a46b8ea9194479b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "19dc38957207e6691e806b378eb62748",
+    "revision": "4f7844b9ba05e9962a2edade6ac155f6",
     "url": "/my-portfolio/index.html"
   },
   {
