659fed82cf956ae5c7539e3eca8673c6c47814dd
shubhanshudhagat <shrivastavanavneeta@gmail.com>
2022-02-28 11:23:58 +0800

    Updates

--- precache-manifest.ebb79827d7cd8127396fbdef6064cb72.js
+++ precache-manifest.ebb79827d7cd8127396fbdef6064cb72.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1011c631f3941cafc54d9c3ca2af79eb",
+    "revision": "8316456baed5e63689333b5b71f73625",
     "url": "/my-portfolio/index.html"
   },
   {
-    "revision": "79f1e103be0ee4f354a8",
+    "revision": "abab0c7dd0c6361544fb",
     "url": "/my-portfolio/static/css/2.6280b9c9.chunk.css"
   },
   {
-    "revision": "c07e49a00828d2f7ea76",
+    "revision": "1c37331cbb002243e3b5",
     "url": "/my-portfolio/static/css/main.c6a06ff3.chunk.css"
   },
   {
-    "revision": "79f1e103be0ee4f354a8",
-    "url": "/my-portfolio/static/js/2.9a2246a4.chunk.js"
+    "revision": "abab0c7dd0c6361544fb",
+    "url": "/my-portfolio/static/js/2.9a426639.chunk.js"
   },
   {
     "revision": "2d3407f67e1ce6854fbe05878e8577b3",
-    "url": "/my-portfolio/static/js/2.9a2246a4.chunk.js.LICENSE.txt"
+    "url": "/my-portfolio/static/js/2.9a426639.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c07e49a00828d2f7ea76",
-    "url": "/my-portfolio/static/js/main.3fb66b5f.chunk.js"
+    "revision": "1c37331cbb002243e3b5",
+    "url": "/my-portfolio/static/js/main.fc42341c.chunk.js"
   },
   {
     "revision": "28d97e6c4183a7c87d51",
