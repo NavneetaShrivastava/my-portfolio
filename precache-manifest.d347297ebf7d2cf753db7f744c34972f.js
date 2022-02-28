1186c33478c08c8a1effdc7a84f83ff1f1d3b738
shubhanshudhagat <shrivastavanavneeta@gmail.com>
2022-02-28 11:43:26 +0800

    Updates

--- precache-manifest.d347297ebf7d2cf753db7f744c34972f.js
+++ precache-manifest.d347297ebf7d2cf753db7f744c34972f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d75eb28261b97f1088cc39b714d26783",
+    "revision": "49c0fe50057791451018c433c4268fb5",
     "url": "/my-portfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/my-portfolio/static/css/2.6280b9c9.chunk.css"
   },
   {
-    "revision": "4a3ef83cedde506e465c",
+    "revision": "7513da8741be8b46e781",
     "url": "/my-portfolio/static/css/main.c6a06ff3.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/my-portfolio/static/js/2.4ea7ad42.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4a3ef83cedde506e465c",
-    "url": "/my-portfolio/static/js/main.25842ca4.chunk.js"
+    "revision": "7513da8741be8b46e781",
+    "url": "/my-portfolio/static/js/main.6f3a27c7.chunk.js"
   },
   {
     "revision": "28d97e6c4183a7c87d51",
