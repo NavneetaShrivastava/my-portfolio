0c8e83559795b9fa69ad030a82ae19675cdab2f8
NavneetaShrivastava <navneetashrivastava@gmail.com>
2022-03-02 00:27:18 +0800

    done

--- src/components/Stackoverflow.js
+++ src/components/Stackoverflow.js
@@ -11,23 +11,53 @@ class Stackoverflow extends Component {
         var answers = this.props.sharedStackoverflowInfo.items.map(function (answers) {
           return (
               <div
-              className="col-sm-12 col-md-6 col-lg-4 mb-4"
+              className=" mb-4"
               key={answers.title}
               style={{ cursor: "pointer" }}
               >
-              <span className="stackoverflow-item  d-block h-100 w-100">
-                <div className="foto h-100 w-100" >
+                  <div className="card ">
+                  <div className="card-header  ">
+                    <span
+                      className="iconify"
+                      data-icon="emojione:red-circle"
+                      data-inline="false"
+                    ></span>{" "}
+                    &nbsp;{" "}
+                    <span
+                      className="iconify"
+                      data-icon="twemoji:yellow-circle"
+                      data-inline="false"
+                    ></span>{" "}
+                    &nbsp;{" "}
+                    <span
+                      className="iconify"
+                      data-icon="twemoji:green-circle"
+                      data-inline="false"
+                    ></span>
+                  </div>
+                  <div
+                    className="card-body foto font-trebuchet text-justify ml-3 mr-3"
+                    style={{
+                      height: "auto",
+                      fontSize: "132%",
+                      lineHeight: "200%",
+                    }}
+                  >
+                   
+                
                   <div className="h-100 w-100">
-                  <h3 className="stackoverflow-title-settings mt-3">
+                  <a href={answers.link} class="card-link font-weight-bold text-dark"><h3 style={{backgroundColor:"#efe1bd"}} className="stackoverflow-title-settings mt-3">
                    {answers.title}?
-                    </h3>
-                  { <p  class="text-black stackoverflow-answer" dangerouslySetInnerHTML={{ __html:(answers.body) }} /> }
-                  <a href={answers.link} class="card-link">View</a>
+                    </h3></a>
+                  { <p class="text-black stackoverflow-answer"  dangerouslySetInnerHTML={{ __html:(answers.body) }} /> }
                     <br />
               
                   </div>
-                </div>
-              </span>
+                
+             
+              </div>
+                  </div>
+             
               </div>
           );
 
@@ -41,13 +71,13 @@ class Stackoverflow extends Component {
             <span> <svg stroke="currentColor" 
             fill="currentColor" 
             stroke-width="0" 
-            viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" style={{ fontSize: "40px", marginRight: "8px" }}>
+            viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" style={{ fontSize: "40px", marginBottom: "20px" }}>
               <path d="M12.412 14.572V10.29h1.428V16H1v-5.71h1.428v4.282h9.984z"></path>
               <path d="M3.857 13.145h7.137v-1.428H3.857v1.428zM10.254 0 9.108.852l4.26 5.727 1.146-.852L10.254 0zm-3.54 3.377 5.484 4.567.913-1.097L7.627 2.28l-.914 1.097zM4.922 6.55l6.47 3.013.603-1.294-6.47-3.013-.603 1.294zm-.925 3.344 6.985 1.469.294-1.398-6.985-1.468-.294 1.397z">
-                </path></svg> <h2 class="ant-typography" style={{ color: "rgb(255, 255, 255)"}}>{sectionName}</h2></span>
+                </path></svg> <span style={{ marginLeft: "-7px"}}class="ant-typography text-black">{sectionName} Stackoverflow</span></span>
           </h1>
-          <div className="col-md-12 mx-auto">
-            <div className="row mx-auto">{answers}</div>
+          <div className="card-columns mx-auto">
+            {answers}
           </div>
         </div>
       </section>
