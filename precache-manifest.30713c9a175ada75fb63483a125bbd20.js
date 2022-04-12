b295172f92d812e78f50d5d8391aa7ceac2c65ee
NavneetaShrivastava <navneetashrivastava@gmail.com>
2022-04-12 14:18:15 +0800

    Updates

---
+++ precache-manifest.30713c9a175ada75fb63483a125bbd20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374b373f67c9bbde1ff0ea0ebb5f3a4a",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "7eaf5212598a47b27b81",
+    "url": "/my-portfolio/static/css/2.9c389199.chunk.css"
+  },
+  {
+    "revision": "a8457d7aca4726971e64",
+    "url": "/my-portfolio/static/css/main.30d8802b.chunk.css"
+  },
+  {
+    "revision": "7eaf5212598a47b27b81",
+    "url": "/my-portfolio/static/js/2.ba121e9b.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "/my-portfolio/static/js/2.ba121e9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8457d7aca4726971e64",
+    "url": "/my-portfolio/static/js/main.8b8ade78.chunk.js"
+  },
+  {
+    "revision": "28d97e6c4183a7c87d51",
+    "url": "/my-portfolio/static/js/runtime-main.9994cf37.js"
+  }
+]);
